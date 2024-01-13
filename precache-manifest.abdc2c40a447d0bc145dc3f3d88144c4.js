252a73d48c786df4a1d612e5407df46a2c38a86d
Nikhil Purswani <nikhilopp66@gmail.com>
2024-01-13 16:08:15 +0530

    Updates

---
+++ precache-manifest.abdc2c40a447d0bc145dc3f3d88144c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2a0afa2ae61ada6093e6f45bb1b6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43fb797915a16ace71fc",
+    "url": "/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "c4d88a3332a9f6746fff",
+    "url": "/static/css/main.4bdf4150.chunk.css"
+  },
+  {
+    "revision": "43fb797915a16ace71fc",
+    "url": "/static/js/2.a40c962d.chunk.js"
+  },
+  {
+    "revision": "1876019e6f8cf7ca83e2",
+    "url": "/static/js/3.0e4cdb98.chunk.js"
+  },
+  {
+    "revision": "c4d88a3332a9f6746fff",
+    "url": "/static/js/main.ed23d91a.chunk.js"
+  },
+  {
+    "revision": "08358a26ce333fe80872",
+    "url": "/static/js/runtime~main.b8f3c4c4.js"
+  }
+]);
