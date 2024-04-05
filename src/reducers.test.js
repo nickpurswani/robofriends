bd3e1282effde1d324a20c063946760219c9fb5f
Nikhil Purswani <nikhilopp66@gmail.com>
2024-04-05 13:46:16 +0530

    circleci

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -7,6 +7,48 @@ describe('searchRobots',()=>{
         expect(reducers.searchRobots(undefined,{})).toEqual({searchField:''})
     })
     it('should return the search state',()=>{
-        expect(reducers.searchRobots({searchField:'a'},{type:type.CHANGE_SEARCH_FIELD,payload:'a'})).toEqual({searchField:'a'})
+        expect(reducers.searchRobots({},{type:type.CHANGE_SEARCH_FIELD,payload:'a'})).toEqual({searchField:'a'})
     })
+})
+describe('RequestRobots',()=>{
+
+    const initialStateRobots = {
+        robots: [],
+        isPending: true
+      }
+it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+    expect(
+      reducers.requestRobots(initialStateRobots, {
+        type: type.REQUEST_ROBOTS_SUCCESS,
+        payload: [{
+            id: '123',
+            name: 'test',
+            email: 'j@jmail.com'
+          }]
+      })
+    ).toEqual(
+      {
+        robots: [{
+          id: '123',
+          name: 'test',
+          email: 'j@jmail.com'
+        }],
+        isPending: false
+      }
+    )
+  })
+  it('should handle REQUEST_ROBOTS_FAILED action', () => {
+    expect(
+      reducers.requestRobots(initialStateRobots, {
+        type: type.REQUEST_ROBOTS_FAILED,
+        payload: 'NOOO'
+      })
+    ).toEqual(
+      {
+        error: 'NOOO',
+        robots: [],
+        isPending: false
+      }
+    )
+  })
 })
